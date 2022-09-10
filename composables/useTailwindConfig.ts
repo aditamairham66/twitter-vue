9995cacc3a90b1e6fc5be7261fe5f10c96895fb2
aditamairham66 <aditamairham66@gmail.com>
2022-09-10 15:57:26 +0700

    first commit

---
+++ composables/useTailwindConfig.ts
@@ -0,0 +1,6 @@
+export function useTailwindConfig () {
+    return {
+        defaultTransition: 'transition ease-in-out duration-350',
+        twitterBorderColor: 'border-white-200 dark:border-gray-700',
+    }
+}
