99fe48904bc02d35c3e598a3b97299fd6a666a39
aditamairham66 <aditamairham66@gmail.com>
2022-09-11 13:51:24 +0700

    add api register

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        home page
+        <MainPageSection title="Home" :loading="loading">
+            page home
+        </MainPageSection>
     </div>
 </template>
 
 <script setup>
-
+const loading = ref(false)
 </script>
 
 <style lang="scss" scoped>
