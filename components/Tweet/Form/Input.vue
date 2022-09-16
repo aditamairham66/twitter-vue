081b4781a6fce82390f3d82eef5affc246303313
aditamairham66 <aditamairham66@gmail.com>
2022-09-16 13:28:14 +0700

    update form

--- components/Tweet/Form/Input.vue
+++ components/Tweet/Form/Input.vue
@@ -3,16 +3,36 @@
         <div class="flex w-12 items-top">
             <img :src="props.user?.profileImage" alt="" class="inline-block w-10 h-10 rounded-full">
         </div>
+
+        <div class="w-full p-2">
+            <textarea 
+                v-model="text" 
+                class="w-full h-10 text-lg text-gray-900 placeholder:text-gray-400 bg-transparent border-0 dark:tex.white focus:ring-0"></textarea>
+        </div>
+
+        <div>
+            <button @click="handleSubmitTweet">Tweet</button>
+        </div>
     </div>
 </template>
 
 <script setup>
+    const text = ref('')
+
     const props = defineProps({
         user: {
             type: Object,
             required: true
         }
     })
+
+    const emits = defineEmits(['onSubmit'])
+
+    const handleSubmitTweet = () => {
+        emits('onSubmit', {
+            text: text.value
+        })
+    }
 </script>
 
 <style lang="scss" scoped>
--- components/Tweet/Form/index.vue
+++ components/Tweet/Form/index.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <TweetFormInput :user="user"/>
+        <TweetFormInput :user="user" @onSubmit="handleTweetSubmit"/>
     </div>
 </template>
 
 <script setup>
     const { useAuthUser } = useAuth()
     const user = useAuthUser()
+
+    const handleTweetSubmit = (data) => {
+        console.log(data)
+    }
 </script>
 
 <style lang="scss" scoped>
