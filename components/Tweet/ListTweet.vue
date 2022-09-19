f0f4720c889a9063359d41f55528b5ddc7560b9f
aditamairham66 <aditamairham66@gmail.com>
2022-09-19 13:17:13 +0700

    add list tweet empty state

--- components/Tweet/ListTweet.vue
+++ components/Tweet/ListTweet.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
 
-        <div 
+        <div v-if="isEmptyArray" class="p-4">
+            <p class="text-center text-gray-500">
+                No tweets 😢
+            </p>
+        </div>
+
+        <div v-else
             class="pb-4 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300" 
+            :class="[twitterBorderColor, defaultTransition]"
             v-for="(row, i) in props.listTweet" 
             :key="i">
             <TweetItem :tweet="row"/>
@@ -12,12 +19,17 @@
 </template>
 
 <script setup>
+    const { twitterBorderColor, defaultTransition } = useTailwindConfig()
+
     const props = defineProps({
         listTweet: {
             type: Array,
             required: true
         }
     })
+
+    const isEmptyArray = computed(() => props.listTweet.length === 0)
+
 </script>
 
 <style lang="scss" scoped>
