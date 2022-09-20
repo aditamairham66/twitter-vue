24656b650e64713bd05880a287dcb388016f0162
aditamairham66 <aditamairham66@gmail.com>
2022-09-20 15:02:28 +0700

    add tweet status per users

--- components/Tweet/ListTweet.vue
+++ components/Tweet/ListTweet.vue
@@ -10,8 +10,8 @@
         <div v-else
             class="pb-4 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300" 
             :class="[twitterBorderColor, defaultTransition]"
-            v-for="(row, i) in props.listTweet" 
-            :key="i">
+            v-for="(row, i) in props.listTweet" :key="i" 
+            @click.native="redirectStatus(row)">
             <TweetItem :tweet="row" compact/>
         </div>
 
@@ -29,6 +29,13 @@
     })
 
     const isEmptyArray = computed(() => props.listTweet.length === 0)
+
+    const redirectStatus = (tweet) => {
+        // navigateTo({
+        //     path: `/status/${tweet.id}`
+        // })
+        window.location.href = `/status/${tweet.id}`
+    }
 
 </script>
 
